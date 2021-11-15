6367204d9166ebb4e8558343ae10c212792e8c45
Mykola Morgunenko <Olli25010@gmail.com>
2021-11-15 14:24:08 +0100

    bugs fixed in cmake directory (PVS and Werr)

--- cmake/defaults/CompilerWarnings.cmake
+++ cmake/defaults/CompilerWarnings.cmake
@@ -1,9 +1,11 @@
 set(MSVC_WARNINGS /W4)
-set(GCC_CLANG_WARNINGS -Wall -Wextra -Wpedantic)
+set(GCC_CLANG_WARNINGS -Wextra)
 
 if (WARNINGS_AS_ERRORS)
+    message("- [custom_cmake] Warnings as errors enabled. You can disable it in CMakeLists.txt")
+
     set(MSVC_WARNINGS ${MSVC_WARNINGS} /WX)
-    set(GCC_CLANG_WARNINGS ${GCC_CLANG_WARNINGS} -Werror -Wvla)
+    set(GCC_CLANG_WARNINGS ${GCC_CLANG_WARNINGS} -Wall -pedantic -Werror -Werror=vla)
 else ()
     set(GCC_CLANG_WARNINGS ${GCC_CLANG_WARNINGS} -Werror=vla)
 endif ()
